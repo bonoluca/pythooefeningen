0141e52d0c07a7c1311f1a92bc08d670983c7d8a
bonoluca <bonoluca0707@gmail.com>
2025-09-25 09:27:20 +0200

    first commit

---
+++ knight_rider_part1.py
@@ -0,0 +1,65 @@
+from gpiozero import LED
+from time import sleep
+Led1 = LED(5)
+Led2 = LED(6)
+Led3 = LED(13)
+Led4 = LED(19)
+Led5 = LED(26)
+Led6 = LED(20)
+Led7 = LED(17)
+Led8 = LED(16)
+delay = 0.1
+while True:
+    Led1.on()
+    sleep(delay)
+    Led1.off()
+    Led2.on()
+    sleep(delay)
+    Led2.off()
+    Led3.on()
+    sleep(delay)
+    Led3.off()
+    Led4.on()
+    sleep(delay)
+    Led4.off()
+    Led5.on()
+    sleep(delay)
+    Led5.off()
+    Led6.on()
+    sleep(delay)
+    Led6.off()
+    Led7.on()
+    sleep(delay)
+    Led7.off()
+    Led8.on()
+    sleep(delay)
+    Led8.off()
+    Led7.on()
+    sleep(delay)
+    Led7.off()
+    Led6.on()
+    sleep(delay)
+    Led6.off()
+    Led5.on()
+    sleep(delay)
+    Led5.off()
+    Led4.on()
+    sleep(delay)
+    Led4.off()
+    Led3.on()
+    sleep(delay)
+    Led3.off()
+    Led2.on()
+    sleep(delay)
+    Led2.off()
+
+    
+
+    
+    
+    
+   
+    
+
+
+
