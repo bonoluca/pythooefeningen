8ccce961e8fb4351bb6c8bf93da20d65e348c50c
bonoluca <bonoluca0707@gmail.com>
2025-09-25 10:30:24 +0200

    les25/09

--- knight_rider_part1.py
+++ knight_rider_part1.py
@@ -1,57 +1,57 @@
-from gpiozero import LED
-from time import sleep
-Led1 = LED(5)
-Led2 = LED(6)
-Led3 = LED(13)
-Led4 = LED(19)
-Led5 = LED(26)
-Led6 = LED(20)
-Led7 = LED(17)
-Led8 = LED(16)
-delay = 0.1
-while True:
-    Led1.on()
-    sleep(delay)
-    Led1.off()
-    Led2.on()
-    sleep(delay)
-    Led2.off()
-    Led3.on()
-    sleep(delay)
-    Led3.off()
-    Led4.on()
-    sleep(delay)
-    Led4.off()
-    Led5.on()
-    sleep(delay)
-    Led5.off()
-    Led6.on()
-    sleep(delay)
-    Led6.off()
-    Led7.on()
-    sleep(delay)
-    Led7.off()
-    Led8.on()
-    sleep(delay)
-    Led8.off()
-    Led7.on()
-    sleep(delay)
-    Led7.off()
-    Led6.on()
-    sleep(delay)
-    Led6.off()
-    Led5.on()
-    sleep(delay)
-    Led5.off()
-    Led4.on()
-    sleep(delay)
-    Led4.off()
-    Led3.on()
-    sleep(delay)
-    Led3.off()
-    Led2.on()
-    sleep(delay)
-    Led2.off()
+from gpiozero import LED  #import LED
+from time import sleep  #import sleep
+Led1 = LED(5)  #een variabelen voor led1
+Led2 = LED(6)  #een variabelen voor led2
+Led3 = LED(13) #een variabelen voor led3
+Led4 = LED(19) #een variabelen voor led4
+Led5 = LED(26) #een variabelen voor led5
+Led6 = LED(20) #een variabelen voor led6
+Led7 = LED(17) #een variabelen voor led7
+Led8 = LED(16) #een variabelen voor led8
+delay = 0.1   #een variabelen voor delay
+while True:  #zolang het true is 
+    Led1.on()    #led gaat aan 
+    sleep(delay)  #delay 
+    Led1.off()     #led gaat uit 
+    Led2.on()  #led gaat aan 
+    sleep(delay) #delay 
+    Led2.off()  #led gaat uit
+    Led3.on() #led gaat aan
+    sleep(delay) #delay 
+    Led3.off()  #led gaat uit
+    Led4.on() #led gaat aan
+    sleep(delay) #delay 
+    Led4.off()  #led gaat uit
+    Led5.on() #led gaat aan
+    sleep(delay) #delay 
+    Led5.off()  #led gaat uit
+    Led6.on() #led gaat aan
+    sleep(delay) #delay 
+    Led6.off()  #led gaat uit
+    Led7.on() #led gaat aan
+    sleep(delay) #delay 
+    Led7.off()  #led gaat uit
+    Led8.on() #led gaat aan
+    sleep(delay) #delay 
+    Led8.off()  #led gaat uit
+    Led7.on() #led gaat aan
+    sleep(delay) #delay 
+    Led7.off()  #led gaat uit
+    Led6.on() #led gaat aan
+    sleep(delay) #delay 
+    Led6.off()  #led gaat uit
+    Led5.on() #led gaat aan
+    sleep(delay) #delay 
+    Led5.off()  #led gaat uit
+    Led4.on() #led gaat aan
+    sleep(delay) #delay 
+    Led4.off()  #led gaat uit
+    Led3.on() #led gaat aan
+    sleep(delay) #delay 
+    Led3.off()  #led gaat uit
+    Led2.on() #led gaat aan
+    sleep(delay) #delay 
+    Led2.off()  #led gaat uit
 
     
 
