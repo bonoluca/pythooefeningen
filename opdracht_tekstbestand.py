15cb45d7f5f8a5e1b3b0835cc4183c76e8c3b38b
bonoluca <bonoluca0707@gmail.com>
2025-10-23 10:41:30 +0200

    oefeningles23/10

--- opdracht_tekstbestand.py
+++ opdracht_tekstbestand.py
@@ -3,26 +3,33 @@ import board
 import adafruit_dht
 from time import gmtime , strftime
 
-
+f = open("tijdbestand.txt", "w")
+f.write("nr    tijd    temp(°C)  vochtigheid(%)\n")
+f.close()
 dhtDevice = adafruit_dht.DHT11(board.D18)
 
-huidige_tijd = ("%S",gmtime())
+
 
 teller = 0 
 
 
 
-
-
-
 while True:
     try:
+        huidige_tijd = strftime (" %H:%M:%S ")
         temperature_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
         print(temperature_c, humidity)
-        print("nr tijd  Temp(°C)  Vochtigheid(%)") 
-        print(teller ,huidige_tijd)
-        teller = teller + 1
+        print(huidige_tijd)
+        
+        f= open("tijdbestand.txt", "a")
+        f.write(f"{teller}")
+        f.write(f"{huidige_tijd}  ")
+        f.write(f"{temperature_c}          ")
+        f.write(f"{humidity}\n")
+        f.close()
+        teller = teller +1
+
 
       
 
