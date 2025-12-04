7a1d3f29621b3e6753b979265207724eb30b7d78
bonoluca <bonoluca0707@gmail.com>
2025-12-04 09:52:20 +0100

    test4

--- ADC_opdracht2.py
+++ ADC_opdracht2.py
@@ -8,11 +8,17 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 from time import  strftime
 import adafruit_dht
 import board
+from gpiozero import LED
 
+adc = MCP3008(chanel=0)
 dhtDevice = adafruit_dht.DHT11(board.D18)
-
+rood = LED(5)
+geel = LED(6)
+blauw = LED(13)
 while True:
     try:
+        voltage = adc.value * 3.3
+        tmp36_c = (voltage * 0.500)*100
         temperature_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
         print(temperature_c, humidity)
