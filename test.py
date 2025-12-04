d6a10052fce6209012034966d21f59005b38361f
bonoluca <bonoluca0707@gmail.com>
2025-12-04 10:28:39 +0100

    test8

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ dht = adafruit_dht.DHT11(DHT_PIN)
 # LEDs (voorbeeld)
 rood = LED(5)
 geel = LED(6)
-blauw = LED(13)
+groen = LED(13)
 
 
 while True:
@@ -45,25 +45,29 @@ while True:
 
     # LEDs en print
     if dht_c is not None:
-        # Metingen ok -> blauw aan
-        blauw.on()
+        # Metingen ok -> groen aan
+        groen.on()
         rood.off()
 
         # Print exact de drie waarden
         print(f"DHT11: {dht_c:.1f} °C | TMP36: {tmp36_c:.1f} °C | Δ: {verschil:+.1f} °C")
-
+        f= open("tijdbestand.txt", "a")
+        f.write(f"{huidige_tijd}  {dht_c:.1f} {tmp36_c:.1f} {verschil:+.1f} ")
+        f.close()
         # Geel aan als verschil groter dan 2°C
         if abs(verschil) > 2.0:
             geel.on()
         else:
             geel.off()
     else:
-        # DHT mislukt -> rood aan, geel/blauw uit
+        # DHT mislukt -> rood aan, geel/groen uit
         rood.on()
         geel.off()
-        blauw.off()
+        groen.off()
         print(f"DHT11: NA     | TMP36: {tmp36_c:.1f} °C | Δ: NA")
         f= open("tijdbestand.txt", "a")
-        f.write(f"{huidige_tijd}  {dht_c:.1f} {tmp36_c:.1f} {verschil:+.1f} ")
+        f.write(f"DHT11: NA     | TMP36: {tmp36_c:.1f} °C | Δ: NA\n")
+        f.close()
+
 
     time.sleep(INTERVAL_SEC)
