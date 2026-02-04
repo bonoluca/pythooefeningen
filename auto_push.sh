ba876cbbcd3663c7272c91f0a169b9b1b22135d0
bonoluca <bonoluca0707@gmail.com>
2026-02-04 09:39:06 +0100

    maak de auto push sh bestand aan

---
+++ auto_push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ga naar de map van je Git-repository
+cd /home/pi/weatherheat-python/ICT-projecten || exit
+
+# Haal eventuele wijzigingen van GitHub op
+git pull origin main --no-rebase
+
+# Voeg nieuwe of gewijzigde bestanden toe
+git add .
+
+# Maak een commit met automatische datum/tijd
+git commit -m "Auto update $(date '+%Y-%m-%d %H:%M:%S')"
+
+# Push naar GitHub
+git push origin main
