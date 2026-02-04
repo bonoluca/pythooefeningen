0da3921826380ea79c36fda03f8ab0c87f14c7ca
bonoluca <bonoluca0707@gmail.com>
2026-02-04 11:46:24 +0100

    eerste aanpassing aan de code van adc test

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ import time
 from datetime import datetime
 from gpiozero import MCP3008
 import adafruit_dht
+import board
 
 # Config
 CSV_BESTAND = "test_temp_log.csv"
 VREF = 3.3
 TMP36_CHANNEL = 0
-DHT_PIN = 4
-DHT_SENSOR =  adafruit_dht.DHT11
+dht = adafruit_dht.DHT11(board.D4)
 
 # Init
 adc = MCP3008(channel=TMP36_CHANNEL)
@@ -30,7 +30,12 @@ while True:
     tmp36_c = (voltage - 0.500) * 100
 
     # DHT11 uitlezen
-    humidity, dht_c =  adafruit_dht.read_retry(DHT_SENSOR, DHT_PIN)
+    try:
+        dht_c = dht.temperature
+        humidity = dht.humidity
+    except RuntimeError:
+        dht_c = None
+        humidity = None
 
     verschil = tmp36_c - dht_c if dht_c is not None else None
 
@@ -45,3 +50,6 @@ while True:
         f.write(f"{datum};{tijd};{dht_c if dht_c else 'NA'};{tmp36_c:.1f};{verschil if verschil else 'NA'}\n")
 
     time.sleep(2)
+    #de code hier onder voorkomt gpio locks bij stoppen met ctrl + c 
+    import atexit 
+    atexit.register(lambda: dht.exit())
