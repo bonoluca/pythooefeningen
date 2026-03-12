de51b1fff7a7ad16fe62f23ae4fc2c2ab8aa1eab
bonoluca <bonoluca0707@gmail.com>
2026-03-12 10:35:56 +0100

    comentaat toevoegen

--- potMeter_oefeningen_motor/potmeter_motor_oef.py
+++ potMeter_oefeningen_motor/potmeter_motor_oef.py
@@ -1,17 +1,15 @@
-from gpiozero import Motor
-from gpiozero import MCP3008
-from time import sleep
+from gpiozero import Motor #importeer de gpiozero library
+from gpiozero import MCP3008 #importeer de mcp3008 library
+from time import sleep #importeer sleep
 
-pot = MCP3008(channel=0)
-motor = Motor(forward=17, backward=14, pwm = True)
+pot = MCP3008(channel=0) #de pot meter op channel 0 zetten
+motor = Motor(forward=17, backward=14, pwm = True)  #zet de forward op pin 17 en backwards op 14 en zet de pwm op true
+ 
+while True: #zolang true is blijf door gaan
+    motor.forward(pot.value)  # laat de motor naar voor draaiden gelijk aan pot meter waarde
+    print(pot.value)  #print de potentiometer value
+    sleep(0.1)  #een sleep voor 0.1 sec
 
-while True:
-    motor.forward(pot.value)
-    print(pot.value)
-    sleep(0.1)
-    motor.backward(pot.value)
-    print(pot.value)
-    sleep(0.1)
 
 
 
