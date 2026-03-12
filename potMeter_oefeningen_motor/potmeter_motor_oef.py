4f20cbf96796252aa02810826bcc2e25a47e81a2
bonoluca <bonoluca0707@gmail.com>
2026-03-12 09:53:50 +0100

    code maken

--- potMeter_oefeningen_motor/potmeter_motor_oef.py
+++ potMeter_oefeningen_motor/potmeter_motor_oef.py
@@ -1,3 +1,15 @@
+from gpiozero import Motor
+from gpiozero import MCP3008
+from time import sleep
 
+pot = MCP3008(channel=0)
+motor = Motor(forward=4, backward=14)
+
+while True:
+    motor.forward()
+    sleep(5)
+    motor.backward()
+    sleep(5)
+    print(pot.value)
 
 
