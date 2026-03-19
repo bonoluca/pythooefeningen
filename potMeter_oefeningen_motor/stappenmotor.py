3a8dc552c2b7bdefdfaf05102c56a376884fabd7
bonoluca <bonoluca0707@gmail.com>
2026-03-19 14:03:33 +0100

    inpu aanmaken

--- potMeter_oefeningen_motor/stappenmotor.py
+++ potMeter_oefeningen_motor/stappenmotor.py
@@ -8,5 +8,10 @@ GpioPins = [18, 23, 24, 25]
 # Declare an named instance of class pass a name and motor type
 mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
 
+vraag_voor_hvl_mm = input("hvl mm wil je draaien ")
+
+rotatie_nr = 2
+
+
 # call the function , pass the parameters
 mymotortest.motor_run(GpioPins , .01, 100, False, False, "half", .05)
