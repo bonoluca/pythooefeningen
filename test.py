09b2380cc055251b37e068798f88614dec5810aa
bonoluca <bonoluca0707@gmail.com>
2026-03-31 09:42:56 +0200

    code gevonded

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
+import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
+GPIO.setup(4, GPIO.IN, GPIO.PUD_UP)
 
-    
+GPIO.wait_for_edge(4, GPIO.FALLING):
+print("button was pressed")
 
+from gpiozero import Button
 
+btn = Button(4)
 
-   
+btn.wait_for_press()
+print("button was pressed")
